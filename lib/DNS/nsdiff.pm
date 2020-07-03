c3de17fc6292477dc50838591f836c16c7828e57
Tony Finch <dot@dotat.at>
2020-07-03 19:57:46 +0100

    dist: a place-holder module for overview docs
    CPAN users generally expect to get started with `perldoc <distname>`

---
+++ lib/DNS/nsdiff.pm
@@ -0,0 +1,51 @@
+# SPDX-License-Identifier: CC0-1.0
+
+package DNS::nsdiff;
+
+our $VERSION = 0.01;
+
+=head1 NAME
+
+DNS::nsdiff - the nsdiff, nspatch, and nsvi scripts
+
+=head1 DESCRIPTION
+
+This is a stub module for overview documentation. There are three
+scripts in the DNS::nsdiff distribution:
+
+=over
+
+=item B<nsdiff>
+
+The B<nsdiff> program examines the old and new versions of a DNS zone,
+and outputs the differences as a script for use by BIND's B<nsupdate>
+program. It provides a bridge between static zone files and dynamic
+updates.
+
+=item B<nspatch>
+
+The B<nspatch> script is a wrapper around C<nsdiff | nsupdate> that
+checks and reports errors in a manner suitable for running from B<cron>.
+
+=item B<nsvi>
+
+The B<nsvi> script makes it easy to edit a dynamic zone.
+
+=back
+
+=head1 VERSION
+
+  This is nsdiff-1.79 <https://dotat.at/prog/nsdiff/>
+
+  Written by Tony Finch <fanf2@cam.ac.uk> <dot@dotat.at>
+  at Cambridge University Information Services.
+  You may do anything with this. It has no warranty.
+  <https://creativecommons.org/publicdomain/zero/1.0/>
+
+=head1 SEE ALSO
+
+nsdiff(1), nspatch(1), nsvi(1), nsupdate(1)
+
+=cut
+
+1;
